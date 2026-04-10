0afcc2ac7673c13f38419576c8892b5ab2587ab3
stephen mason <stephenmason@stephens-MBP.OleLan>
2026-04-10 14:54:26 +0200

    chore: update header section to pull on a Navigation pattern

--- web/app/themes/vanilla-hair-and-beauty/resources/views/sections/header.blade.php
+++ web/app/themes/vanilla-hair-and-beauty/resources/views/sections/header.blade.php
@@ -1,11 +1,44 @@
-<header class="banner">
-  <a class="brand" href="{{ home_url('/') }}">
-    @if (has_custom_logo())
-      {!! get_custom_logo() !!}
-    @else
-      {!! $siteName !!}
-    @endif
-  </a>
+@php
+    global $wpdb;
 
-  @include('partials.navigation')
+    $slug = 'Navigation';
+
+    $header = $wpdb->get_row($wpdb->prepare(
+        "SELECT ID FROM {$wpdb->posts}
+         WHERE post_type = 'wp_block'
+         AND post_status IN ('publish', 'inherit')
+         AND post_title = %s",
+        $slug
+    ));
+
+    $headerId = $header ? (int) $header->ID : 0;
+
+    if (! $headerId ) {
+        $header = $wpdb->get_row($wpdb->prepare(
+            "SELECT ID FROM {$wpdb->posts}
+             WHERE post_type = 'wp_block'
+             AND post_status IN ('publish', 'inherit')
+             AND post_title = %s",
+            'header_EN'
+        ));
+        $headerId = $header ? (int) $header->ID : 0;
+    }
+
+    if ($headerId) {
+        $block = [
+            'blockName'    => 'core/block',
+            'attrs'        => ['ref' => $headerId],
+            'innerHTML'    => '',
+            'innerContent' => [],
+            'innerBlocks'  => [],
+        ];
+
+        $headerContent = render_block($block);
+    }
+@endphp
+
+@if ($headerId)
+<header class="header-theme">
+  {!! $headerContent !!}
 </header>
+@endif
